4fea33855d58c0b07330ef1c7bc48b314907cb74
KRMokgara <pablobotek808@gmail.com>
2022-04-21 17:03:33 +0200

    Initital Solution Setup

---
+++ Q-StateOnline/Q-StateOnline.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Q_StateOnline.UI.MvcApplication" Language="C#" %>
---
+++ Q-StateOnline/Q-StateOnline.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Q_StateOnline.UI.Startup))]
+namespace Q_StateOnline.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
