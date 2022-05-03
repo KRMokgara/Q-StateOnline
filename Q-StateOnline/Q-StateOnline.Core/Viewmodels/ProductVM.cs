b3f6283d7147a8d71c4314437e52e0f035bd55b9
19pct013 <pablobotek808@gmail.com>
2022-05-03 15:23:25 +0200

    Added ProductVM, IRepository and Update product controller

---
+++ Q-StateOnline/Q-StateOnline.Core/Viewmodels/ProductVM.cs
@@ -0,0 +1,15 @@
+﻿using Q_StateOnline.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_StateOnline.Core.Viewmodels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        public IEnumerable<ProductCategory> productCategories { get; set; }
+    }
+}
---
+++ Q-StateOnline/Q-StateOnline.UI/Controllers/ProductController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Q_StateOnline.Core.Contracts;
+using Q_StateOnline.Core.Models;
+using Q_StateOnline.DataAccess.InMemory;
+
+namespace Q_StateOnline.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+        public ProductController()
+        {
+            ProductRepository context;
+
+            public ProductController()
+            {
+                context = new ProductRepository();
+            }
+
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+        public ActionResult Create()
+        {
+            ProductVM viewModel = new ProductVM();
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                ProductVM viewModel = new ProductVM();
+                viewModel.Product = product;
+                viewModel.Productcategories = productCategories.Collection();
+                return View(product);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product prod = context.Find(Id);
+            if(prod == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                prod.Category = product.Category;
+                prod.Descriptin = product.Descriptin;
+                prod.Image = product.Image;
+                prod.Name = product.Name;
+                prod.Price = product.Price;
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Delete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if(productToDelete == null)
+            {
+                return View(productToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if(productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
